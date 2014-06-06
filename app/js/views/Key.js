4c1b7a7cc485aba62d33eb8d07e0d7c164e4add5
Miro Nieminen <miro.nieminen@futurice.com>
2014-06-06 17:36:07 +0300

    Create base class for Key and Gun ItemViews

--- app/js/views/Key.js
+++ app/js/views/Key.js
@@ -2,31 +2,19 @@ define([
     'jquery',
     'underscore',
     'marionette',
+    'views/ButtonView',
     'text!templates/key.html'
 ], function(
     $,
     _,
     Marionette,
+    ButtonView,
     template
     ) {
 
-    return Backbone.Marionette.ItemView.extend({
+    return ButtonView.extend({
         template: _.template(template),
         tagName: 'button',
-        className: 'key',
-
-        events: {
-            "click": "onClick"
-        },
-
-        initialize: function() {
-            this.model.set('isSelected', false);
-        },
-
-        onClick: function(e) {
-            this.model.set('isSelected', !this.model.get('isSelected'));
-            $(this.el).toggleClass('selected');
-
-        }
+        className: 'key'
     });
 });
