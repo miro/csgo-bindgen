366a26468efefa51f51cd6877b88c3b119901b6a
Miro Nieminen <miro.nieminen@futurice.com>
2014-06-09 19:28:36 +0300

    Add comment about the purpose

--- app/js/views/Staging.js
+++ app/js/views/Staging.js
@@ -12,6 +12,9 @@ define([
     StagingGun
     ) {
 
+    // This view is used to "stage" binding
+    // User selects weapons to the staging area, and after pressing "bind",
+    // weapons from staging area comes to the actual binding
     return Backbone.Marionette.CompositeView.extend({
         template: _.template('<div id="staging"></div>'),
         className: 'staging-view',
